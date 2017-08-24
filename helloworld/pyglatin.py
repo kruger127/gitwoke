b96abb540b64ca2f210951cb26fb8aa964f01a03
Krunal <krunal.darji@isprime.com>
2017-08-24 05:59:03 -0400

    txt

--- helloworld/pyglatin.py
+++ helloworld/pyglatin.py
@@ -2,7 +2,7 @@
 
 #pyglatin project
 
-print "Welcome to PygLatin"
+print ("Welcome to PygLatin")
 
 #def run():
 original = raw_input("Enter a random word here: ")
@@ -10,6 +10,6 @@ if len(original) > 0 and original.isalpha():
 	pyg = "ay"
 	print (original[1:]) + (original[:1]) + pyg
 else:
-	print "invalid entry!!"
+	print ("invalid entry!!")
 		
 #done
